628ecab1333436c38675821309f38bb4a48649a3
lecav30 <lecav30@outlook.com>
2024-06-14 15:59:10 -0500

    feat: add resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,11 +1,9 @@
+import PDFViewer from "@/components/pdfviewer";
+
 export default function Resume() {
   return (
     <>
-      <iframe
-        title="Resume"
-        src="../../assets/pdf/Resume.pdf"
-        className="fixed top-0 left-0 w-screen h-screen"
-      />
+      <PDFViewer file="/Resume.pdf" />
     </>
   );
 }
