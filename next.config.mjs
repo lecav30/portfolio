89a06fdb64a29eb2d89591b7ddd72c61206ddd63
lecav30 <lecav30@outlook.com>
2024-06-20 13:27:51 -0500

    refactor: replace react-pdf to react-pdf-viewer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-}
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/components/pdfviewer/index.tsx
+++ src/components/pdfviewer/index.tsx
@@ -1,126 +1,18 @@
 "use client";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
-
-export default function PDFViewer(props: any) {
-  const [numPages, setNumPages] = useState<number>(0);
-  const [pageNumber, setPageNumber] = useState<number>(1); // start on first page
-  const [loading, setLoading] = useState(true);
-  const [pageWidth, setPageWidth] = useState(0);
-
-  function onDocumentLoadSuccess({
-    numPages: nextNumPages,
-  }: {
-    numPages: number;
-  }) {
-    setNumPages(nextNumPages);
-  }
-
-  function onPageLoadSuccess() {
-    if (typeof window === "undefined") return;
-    setPageWidth(window.innerWidth);
-    setLoading(false);
-  }
-
-  const options = {
-    cMapUrl: "cmaps/",
-    cMapPacked: true,
-    standardFontDataUrl: "standard_fonts/",
-  };
-
-  // Go to next page
-  function goToNextPage() {
-    setPageNumber((prevPageNumber) => prevPageNumber + 1);
-  }
-
-  function goToPreviousPage() {
-    setPageNumber((prevPageNumber) => prevPageNumber - 1);
-  }
-
+const PDFViewer = (props: any) => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
-    <>
-      <Nav pageNumber={pageNumber} numPages={numPages} />
-      <div
-        hidden={loading}
-        style={{ height: "calc(100vh - 64px)" }}
-        className="flex items-center"
-      >
-        <div
-          className={`flex items-center justify-between w-full absolute z-10 px-2`}
-        >
-          <button
-            onClick={goToPreviousPage}
-            disabled={pageNumber <= 1}
-            className="relative h-[calc(100vh - 64px)] px-2 py-24 text-gray-400 hover:text-gray-50 focus:z-20"
-          >
-            <span className="sr-only">Previous</span>
-            <ChevronLeft className="h-10 w-10" aria-hidden="true" />
-          </button>
-          <button
-            onClick={goToNextPage}
-            disabled={pageNumber >= numPages!}
-            className="relative h-[calc(100vh - 64px)] px-2 py-24 text-gray-400 hover:text-gray-50 focus:z-20"
-          >
-            <span className="sr-only">Next</span>
-            <ChevronRight className="h-10 w-10" aria-hidden="true" />
-          </button>
-        </div>
-
-        <div className="h-full flex justify-center mx-auto">
-          <Document
-            file={props.file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-            renderMode="canvas"
-          >
-            <Page
-              key={pageNumber}
-              pageNumber={pageNumber}
-              renderAnnotationLayer={false}
-              renderTextLayer={false}
-              onLoadSuccess={onPageLoadSuccess}
-              onRenderError={() => setLoading(false)}
-              width={Math.max(pageWidth * 0.8, 390)}
-            />
-          </Document>
-        </div>
-      </div>
-    </>
+    <div className="h-screen w-screen">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
+      </Worker>
+    </div>
   );
-}
+};
 
-function Nav({
-  pageNumber,
-  numPages,
-}: {
-  pageNumber: number;
-  numPages: number;
-}) {
-  return (
-    <nav className="bg-black">
-      <div className="mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <p className="text-xl font-bold tracking-tighter text-white">
-                Résumé
-              </p>
-            </div>
-          </div>
-          <div
-            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto
-            sm:ml-6 sm:pr-0"
-          >
-            <div className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">
-              <span>{pageNumber}</span>
-              <span className="text-gray-400"> / {numPages}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-}
+export default PDFViewer;
