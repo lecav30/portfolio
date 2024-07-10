72781f1f55779793ec0c4ba97cc44a1e99fc5506
lecav30 <lecav30@outlook.com>
2024-07-10 15:08:12 -0500

    zoom to fit page

--- src/components/pdfviewer/index.tsx
+++ src/components/pdfviewer/index.tsx
@@ -3,13 +3,18 @@ import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { SpecialZoomLevel } from "@react-pdf-viewer/core";
 
 const PDFViewer = (props: any) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="h-screen w-screen">
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
+        <Viewer
+          fileUrl={props.file}
+          plugins={[defaultLayoutPluginInstance]}
+          defaultScale={SpecialZoomLevel.PageFit}
+        />
       </Worker>
     </div>
   );
