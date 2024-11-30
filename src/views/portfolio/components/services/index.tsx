13de2642497e297bc40dcfa1aa673916c6e9012d
lecav30 <lecav30@outlook.com>
2024-11-30 12:35:48 -0500

    feat: update services

--- src/views/portfolio/components/services/index.tsx
+++ src/views/portfolio/components/services/index.tsx
@@ -14,7 +14,7 @@ const Services = () => {
     >
       <Card
         title="Frontend Development"
-        description="My background in frontend development began in 2021 at the university, where I focused on the Angular and VueJS frameworks. Currently, I also have practical experience in ReactJS and NextJS through my professional work."
+        description="My journey in frontend development began in 2021 during university, where I focused on frameworks like Angular and Vue.js. Since then, I have gained practical experience with React.js and Next.js through my professional work."
         skills={[
           "HTML",
           "CSS",
@@ -32,7 +32,7 @@ const Services = () => {
       </Card>
       <Card
         title="Backend Development"
-        description="My foundational knowledge in backend development was established in 2021 through my university studies, primarily focusing on the ASP.NET and Spring Boot frameworks. Additionally, I gained hands-on experience with Flask while working on small university projects."
+        description="My foundational knowledge in backend development was established in 2021 during my university studies, where I primarily focused on frameworks like ASP.NET and Spring Boot. Additionally, I gained hands-on experience with Flask while working on small-scale university projects."
         skills={[
           "ASP.NET - C#",
           "SprintBoot - Java",
@@ -46,7 +46,7 @@ const Services = () => {
       </Card>
       <Card
         title="Mobile Development"
-        description="I embarked on my mobile development journey in 2022 at the university, mastering the Flutter framework. Furthermore, I've gained practical experience in React Native through my professional endeavors."
+        description="I began my mobile development journey in 2022 during university, where I focused on mastering the Flutter framework. Additionally, I have gained practical experience with React Native through my professional work."
         skills={["Flutter - Dart", "React Native"]}
       >
         <Mobile />
